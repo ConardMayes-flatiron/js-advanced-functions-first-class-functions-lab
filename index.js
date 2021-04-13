6362aaf0b0c7a624cffd9160fd924c35c2014baf
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2021-04-13 01:36:27 -0500

    commit tree 6f81dadfaa812763df4180c108e0fcd475e50b24

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = (drivers=[]) => drivers.slice(0,2);
+
+const returnLastTwoDrivers = (drivers=[]) => drivers.slice(-2);
+
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers]);
+
+const createFareMultiplier = num => {
+    return function(fare){
+        return fare*num;
+    }
+}
+
+function fareDoubler(fare) { 
+    return createFareMultiplier(2)(fare);
+
+}
+
+
+function fareTripler(fare) { 
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(driverList, option){
+    return option(driverList);
+}
+done();
